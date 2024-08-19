c37cc035e337aef4e5d70a91a352fe3b0e46bbfb
Valeryio <tommagoma23@gmail.com>
2024-08-19 12:18:35 +0000

    Update the task 2

---
+++ var_config.sh
@@ -0,0 +1,8 @@
+#!/bin/env bash
+
+export HBNB_MYSQL_USER="root"
+# export HBNB_MYSQL_PWD="hbnb_dev_db_pwd"
+export HBNB_MYSQL_PWD="Val_Ery28!"
+export HBNB_MYSQL_HOST="localhost"
+export HBNB_MYSQL_DB="hbnb_dev_db"
+export HBNB_ENV="tes"
